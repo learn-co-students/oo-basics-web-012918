0a480c2479866dd266522aa399ba61137f4977dd
github username <github email address>
2018-01-31 11:45:42 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,51 @@
 
 
+# Notes to transcribe to OneNote
+# OOP Models real life, to an extent
+#Example:
+# Phone Call between 2 people?
+# caller, receivier, duration, and a Cost_per_minute
+# in the real world, that is not the case!!!!!
+
+
+#book.rb
+class Book
+
+#Initialize for our title
+def initialize(title)
+  @title = title
+end
+#Getter for our title that was initialized
+def title
+  @title
+end
+
+#Setter method for our Author
+  def author=(author)
+    @author = author
+  end
+#Getter method for our Author
+  def author
+    @author
+  end
+
+    def page_count=(pages)
+      @page_count = pages
+    end
+    def page_count
+      @page_count
+    end
+
+      def genre=(genre)
+        @genre = genre
+      end
+      def genre
+        @genre
+      end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+
+end
