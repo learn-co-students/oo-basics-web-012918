ee7305546d6c2f80c234edb52f3e066ef838bdd5
github username <github email address>
2018-01-31 13:26:39 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -9,43 +9,21 @@
 
 
 #book.rb
-class Book
 
-#Initialize for our title
-def initialize(title)
-  @title = title
-end
-#Getter for our title that was initialized
-def title
-  @title
-end
 
-#Setter method for our Author
-  def author=(author)
-    @author = author
-  end
-#Getter method for our Author
-  def author
-    @author
-  end
 
-    def page_count=(pages)
-      @page_count = pages
-    end
-    def page_count
-      @page_count
+  class Book
+    attr_accessor :author, :page_count, :genre
+
+    def initialize(title)
+      @title = title
     end
 
-      def genre=(genre)
-        @genre = genre
-      end
-      def genre
-        @genre
-      end
+    def title
+      @title
+    end
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
+    def turn_page
+      puts "Flipping the page...wow, you read fast!"
+    end
   end
-
-
-end
